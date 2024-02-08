2410cbd6e6a8f84b0d4b1734f4607ed0da4026a0
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-08 19:46:08 +0100

    testing Table Contents

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -1,5 +1,6 @@
 import { render, screen, within } from '@testing-library/react';
 import UserList from './UserList';
+
 const users = [
 	{ name: 'Jane', email: 'jane@gmail.com' },
 	{ name: 'Irene', email: 'irene@gmail.com' },
@@ -21,4 +22,21 @@ test('render one row per user 1st approach 2nd approach', () => {
 	// eslint-disable-next-line
 	const rows = container.querySelectorAll('tbody tr');
 	expect(rows).toHaveLength(3);
+});
+
+test('render the email and name of each user', () => {
+	render(<UserList users={users} />);
+	// screen.logTestingPlaygroundURL();
+
+	for (const user of users) {
+		const name = screen.getByRole('cell', { name: user.name });
+		const email = screen.getByRole('cell', { name: user.email });
+
+		expect(name).toBeInTheDocument();
+		expect(email).toBeInTheDocument();
+	}
+
+	// users.forEach((user) => {
+
+	// });
 });
