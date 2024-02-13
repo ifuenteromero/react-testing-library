0b5ab67d3cf9fe03b3d1d277c26f6f0d2dd03e77
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-13 17:23:33 +0100

    More on single queries

--- src/ColorList.test.js
+++ src/ColorList.test.js
@@ -14,3 +14,25 @@ test('getByRole, queryByRole, findByRole finding 0 elements', async () => {
 	}
 	expect(errorThrown).toBe(true);
 });
+
+test('getByRole, queryByRole, findByRole when they find 1 element', async () => {
+	render(<ColorList />);
+	expect(screen.getByRole('list')).toBeInTheDocument();
+	// eslint-disable-next-line testing-library/prefer-presence-queries
+	expect(screen.queryByRole('list')).toBeInTheDocument();
+	expect(await screen.findByRole('list')).toBeInTheDocument();
+});
+
+test('getByRole, queryByRole, findByRole when they find > 1 element', async () => {
+	render(<ColorList />);
+	expect(() => screen.getByRole('listitem')).toThrow();
+	expect(() => screen.queryByRole('listitem')).toThrow();
+
+	let thrownError = false;
+	try {
+		await screen.findByRole('listitem');
+	} catch (error) {
+		thrownError = true;
+	}
+	expect(thrownError).toBe(true);
+});
