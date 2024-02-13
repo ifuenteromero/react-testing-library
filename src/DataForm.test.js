6917948e8a4b03593db19581ce85132737173b7c
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-13 19:37:51 +0100

    when to use each suffix

---
+++ src/DataForm.test.js
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import DataForm from './DataForm';
+
+test('selecting different elements', () => {
+	render(<DataForm />);
+	const elements = [
+		screen.getByRole('button'),
+		screen.getByText(/enter/i),
+
+		screen.getByLabelText(/email/i),
+		screen.getByPlaceholderText('Red'),
+		screen.getByDisplayValue('asdf@asdf.com'),
+		screen.getByAltText('data'),
+		screen.getByTitle(/ready to submit/i),
+
+		screen.getByTestId('image wrapper'),
+	];
+	elements.forEach((el) => {
+		expect(el).toBeInTheDocument();
+	});
+});
