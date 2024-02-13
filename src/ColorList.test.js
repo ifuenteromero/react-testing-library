48aa6074c3c664e4c82e01c88e27c3edeae5c9b5
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-13 17:40:42 +0100

    Multiple elements variations

--- src/ColorList.test.js
+++ src/ColorList.test.js
@@ -36,3 +36,31 @@ test('getByRole, queryByRole, findByRole when they find > 1 element', async () =
 	}
 	expect(thrownError).toBe(true);
 });
+
+test('getAllBy, queryAllBy, findAllBy finding 0 elements', async () => {
+	render(<ColorList />);
+	expect(() => screen.getAllByRole('textbox')).toThrow();
+	expect(screen.queryAllByRole('textbox')).toHaveLength(0);
+
+	let thrownError = false;
+	try {
+		await screen.findAllByRole('textbox');
+	} catch (error) {
+		thrownError = true;
+	}
+	expect(thrownError).toBe(true);
+});
+
+test('getAllBy, queryAllBy, findAllBy finding 1 element', async () => {
+	render(<ColorList />);
+	expect(screen.getAllByRole('list')).toHaveLength(1);
+	expect(screen.queryAllByRole('list')).toHaveLength(1);
+	expect(await screen.findAllByRole('list')).toHaveLength(1);
+});
+
+test('getAllBy, queryAllBy, findAllBy finding > 1 elements', async () => {
+	render(<ColorList />);
+	expect(screen.getAllByRole('listitem')).toHaveLength(3);
+	expect(screen.queryAllByRole('listitem')).toHaveLength(3);
+	expect(await screen.findAllByRole('listitem')).toHaveLength(3);
+});
